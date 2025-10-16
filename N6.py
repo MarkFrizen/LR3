d0e162a1ddd0098ca1d322246e400904fc664171
mark <frizenmarkv@gmail.com>
2025-10-16 22:54:11 +0300

    Корректировка

--- N6.py
+++ N6.py
@@ -1,14 +1,7 @@
+
 class MathUtils:
     @staticmethod
     def square(number: float) -> float:
         return number ** 2
-    @staticmethod
-    def add(a: float, b: float) -> float:
-        return a + b
-    @staticmethod
-    def multiply(a: float, b: float) -> float:
-        return a * b
 if __name__ == "__main__":
     print(f"Квадрат числа 4: {MathUtils.square(4)}")
-    print(f"Сумма 3 и 7: {MathUtils.add(3, 7)}")
-    print(f"Произведение 5 и 6: {MathUtils.multiply(5, 6)}")
--- N9_2.py
+++ N9_2.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
 class Animal(ABC):
     @abstractmethod
-    def make_sound(self) -> None:
+    def make_sound(self):
         pass
 class Dog(Animal):
-    def make_sound(self) -> None:
+    def make_sound(self):
         print("Гав!")
 class Cat(Animal):
-    def make_sound(self) -> None:
+    def make_sound(self):
         print("Мяу!")
 if __name__ == "__main__":
     dog = Dog()
